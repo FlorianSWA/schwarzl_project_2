b6373902953147d2f057be8328a518daeb75fd66
Florian Schwarzl <schwarzl.florian@student.htlwrn.ac.at>
2021-02-16 08:08:37 +0100

    Move tests to main file, add used software to README.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # schwarzl_project_2
 NVS Projekt 2, Schwarzl Florian
+
+
+## Verwendete Software
+* (asio)[https://think-async.com/Asio/]
+* (spdlog)[https://github.com/gabime/spdlog]
+* (CLI11)[https://github.com/CLIUtils/CLI11]
+* (Google Protocol Buffer)[https://developers.google.com/protocol-buffers/]
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,49 @@
+/*
+Author: Florian Schwarzl
+Date: 2021-02-01
+*/
 
 #include <iostream>
 #include <thread>
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "dummy.pb.h"
+#include "CLI11.hpp"
 
 using namespace std;
 
-//int main(int argc, char* argv[]) {
-int main() {
-    thread t{[]{ cout << "Hello"; }};
-    t.join();
-    cout << " world!" << endl;
+int main(int argc, char* argv[]) {
+
+    CLI::App app("Simulate the distributed algorithm for synchronization");
+
+    bool use_logging{false};
+    bool log_level_debug{false};
+
+    auto log_flag{app.add_flag("-l, --log", use_logging, "Write log file dist_sync_log.log")};
+    app.add_flag("-d, --debug", log_level_debug, "Set log level to debug.")->needs(log_flag);
+
+    CLI11_PARSE(app, argc, argv);
+/*
+    shared_ptr<spdlog::logger> file_logger{spdlog::basic_logger_mt("basic_logger", "./dist_sync_log.log")};
+
+    if (use_logging) {
+        if (log_level_debug) {
+            file_logger->set_level(spdlog::level::debug);
+            file_logger->flush_on(spdlog::level::debug);
+        } else {
+            file_logger->set_level(spdlog::level::info);
+            file_logger->flush_on(spdlog::level::info);
+        }
+    } else {
+        file_logger->set_level(spdlog::level::off);
+        file_logger->flush_on(spdlog::level::off);
+    }
+*/
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    Dummy *d = new Dummy;
+    d->set_text("Hello World");
+    cout << d->text() << endl;
+    delete d;
+    google::protobuf::ShutdownProtobufLibrary();
     return 0;
 }
