6ab36aab39ee5f278f173ed9244879d637e1a93c
Florian Schwarzl <schwarzl.florian@student.htlwrn.ac.at>
2021-02-21 15:55:04 +0100

    Format output with color.

--- include/node.h
+++ include/node.h
@@ -16,11 +16,19 @@ Desc: Class representing a node in the simulated network
 class Node {
 
   private:
+    // Unique Id of this node
     int id;
+
+    // Port number of this node
     int port{9999};
+
+    // vector with the port numbers of all nodes
     std::vector<int> neighbours;
+
+    // spdlog file logger
     std::shared_ptr<spdlog::logger> logger;
 
+    // send message to all nodes (including self)
     void send_message(std::string message, int interval);
 
   public:
@@ -30,7 +38,8 @@ class Node {
         neighbours = neighbours_;
         logger = spdlog::get("file_logger");
     };
-        
+    
+    // handle connection request on port
     void serve_request(asio::ip::tcp::socket&& sock);
 
     void operator()();
--- src/node.cpp
+++ src/node.cpp
@@ -6,9 +6,11 @@ Desc: Class representing a node in the simulated network
 */
 
 #include <thread>
+#include <random>
 #include "node.h"
 #include "asio.hpp"
 #include "spdlog/fmt/fmt.h"
+#include "spdlog/fmt/bundled/color.h"
 
 
 
@@ -16,12 +18,19 @@ using namespace asio::ip;
 using namespace std;
 
 void Node::operator()() {
-    thread sender_thd{&Node::send_message, this, "Hello from Node " + to_string(id), 5};
+    random_device rd{};
+    mt19937 gen{rd()};
+    uniform_int_distribution<int> dis{3, 5};
+
+    string msg{"Hello from Node " + to_string(id)};
+    thread sender_thd{&Node::send_message, this, msg, dis(gen)};
     sender_thd.detach();
+
     asio::io_context ctxt;
     tcp::endpoint ep = tcp::endpoint(tcp::v4(), port);
     tcp::acceptor acceptor{ctxt, ep};
     acceptor.listen();
+
     while (true) {
         tcp::socket sock{ctxt};
         acceptor.accept(sock);
@@ -36,7 +45,7 @@ void Node::serve_request(tcp::socket&& sock) {
     string reply;
     istream is{&buf};
     getline(is, reply);
-    fmt::print("Recieved: " + reply + "\n");
+    fmt::print("[{}] Recieved: " + reply + "\n", format(fg(fmt::color::royal_blue), "Node " + to_string(id)));
     sock.close();
 }
 
@@ -45,10 +54,13 @@ void Node::send_message(string message, int interval) {
         for (size_t i{0}; i < neighbours.size(); i++) {
             asio::ip::tcp::iostream strm{"localhost", to_string(neighbours[i])};
             if (strm) {
-                spdlog::info("Connected successfully.");
+                logger->info("Opened connection between Node {} (sender) and Node {} (reciever).", id, i);
+
                 strm << message << "\n";
-                fmt::print("Node {} recieved message: {}\n", id, message);
+                fmt::print("[{}] Node {} recieved message: {}\n", format(fg(fmt::color::royal_blue), "Node " + to_string(id)), id, message);
+
                 strm.close();
+                logger->info("Closed connection between Node {} (sender) and Node {} (reciever).", id, i);
             } else {
                 logger->error("Error occured while connecting: {}", strm.error().message());
             }
